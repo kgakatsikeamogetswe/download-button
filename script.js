5ff68ed6703ff2949714d604d1f2e14f1adca1e3
Keamogetswe Kgakatsi <125869725+kgakatsikeamogetswe@users.noreply.github.com>
2024-04-03 11:18:43 +0200

    script

--- script.js
+++ script.js
@@ -9,7 +9,10 @@ download.addEventListener('click', function(event){
         degree += 1;
         if(degree > 100){
             clearInterval(interval)
-            number.innerHTML = '<i class="fa-solid fa-check"></i>'
+            number.innerHTML = '<i class="fa-solid fa-check"></i><br><small>Done!</small>';
+            return;
         }
-    })
+        container.style.background = ` conic-gradient(#535353 ${degree}%, #000000 0)`;
+        number.innerHTML = degree + '<span>%</span>'
+    }, 100)
 })
